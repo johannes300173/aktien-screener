9b3215f17cbcc9141ff98e1c0941a68ffdd77127
johannes300173 <johannes33333@gmail.com>
2026-02-09 18:26:12 +0100

    Create tickers.py

---
+++ tickers.py
@@ -0,0 +1,20 @@
+DAX = {
+    "BMW.DE": "BMW",
+    "BAS.DE": "BASF",
+    "VOW3.DE": "Volkswagen"
+}
+
+DOW_JONES = {
+    "IBM": "IBM",
+    "CVX": "Chevron",
+    "MMM": "3M"
+}
+
+SP500 = {
+    "INTC": "Intel"
+}
+
+NIKKEI = {
+    "7203.T": "Toyota",
+    "6758.T": "Sony"
+}
